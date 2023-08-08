9671650999b6a66a46dd5700e2f43266da9760c5
imneryy <imneryy.dev@gmail.com>
2023-08-08 10:08:14 -0300

    atualizacao footer card state

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,6 +5,11 @@ import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Sun from "../../assets/solDia.svg";
 import SunNubled from "../../assets/solNublado.svg";
 import Moon from "../../assets/luaNoite.svg";
+import RainingSun from "../../assets/10d.svg";
+import LightningSun from "../../assets/11d.svg";
+import ColdSun from "../../assets/13d.svg";
+import Nuvens from "../../assets/nuvens.svg";
+
 import { useState, useEffect, ReactNode } from "react";
 
 export function Home() {
@@ -16,22 +21,23 @@ export function Home() {
   ]);
   const [imageIcon, setImageIcon] = useState<ReactNode | null>(null);
   const [imageIconCard, setImageIconCard] = useState<ReactNode | null>(null);
+
   const setData = () => {
     const time = new Date().getHours();
-    // let message = "";
+
     if (time > 4) {
       setInfoTextUser("Bom dia, Nery");
       setInfoClima("26°C");
-      setContainerGradient(["#ffac6f", "#ffe467"]);
+      setContainerGradient(["#ffe467", "#ffac6f"]);
       setImageIcon(<Sun width={200} height={200} />);
-      setImageIconCard(<Moon width={40} height={40} />);
+      setImageIconCard(<Sun width={40} height={40} />);
     }
     if (time > 11) {
       setInfoTextUser("Boa tarde, Nery");
       setInfoClima("31°C");
       setContainerGradient(["#292A4E", "#715C77", "#C75C2E"]);
       setImageIcon(<SunNubled width={200} height={200} />);
-      setImageIconCard(<Moon width={40} height={40} />);
+      setImageIconCard(<SunNubled width={40} height={40} />);
     }
     if (time > 17) {
       setInfoTextUser("Boa noite, Nery");
@@ -68,33 +74,38 @@ export function Home() {
         <Text style={styles.footerText}>Previsão para os próximos 7 dias</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
           <View style={styles.footerCardFirst}>
-            <Text style={styles.footerCardTitle}>Seg</Text>
+            <Text style={styles.footerCardTitle}>Hoje</Text>
             {imageIconCard}
+            <Text style={styles.footerCardPreview}>{infoClima}</Text>
+          </View>
+          <View style={styles.footerCard}>
+            <Text style={styles.footerCardTitle}>Qua</Text>
+            <SunNubled width={40} height={40} />
+            <Text style={styles.footerCardPreview}>17°C</Text>
+          </View>
+          <View style={styles.footerCard}>
+            <Text style={styles.footerCardTitle}>Qui</Text>
+            <RainingSun width={40} height={40} />
+            <Text style={styles.footerCardPreview}>19°C</Text>
+          </View>
+          <View style={styles.footerCard}>
+            <Text style={styles.footerCardTitle}>Sex</Text>
+            <LightningSun width={40} height={40} />
+            <Text style={styles.footerCardPreview}>19°C</Text>
+          </View>
+          <View style={styles.footerCard}>
+            <Text style={styles.footerCardTitle}>Sab</Text>
+            <ColdSun width={40} height={40} />
+            <Text style={styles.footerCardPreview}>19°C</Text>
+          </View>
+          <View style={styles.footerCard}>
+            <Text style={styles.footerCardTitle}>Dom</Text>
+            <Nuvens width={40} height={40} />
             <Text style={styles.footerCardPreview}>19°C</Text>
           </View>
           <View style={styles.footerCard}>
             <Text style={styles.footerCardTitle}>Seg</Text>
-            {imageIconCard}
-            <Text style={styles.footerCardPreview}>19°C</Text>
-          </View>
-          <View style={styles.footerCard}>
-            <Text style={styles.footerCardTitle}>Seg</Text>
-            {imageIconCard}
-            <Text style={styles.footerCardPreview}>19°C</Text>
-          </View>
-          <View style={styles.footerCard}>
-            <Text style={styles.footerCardTitle}>Seg</Text>
-            {imageIconCard}
-            <Text style={styles.footerCardPreview}>19°C</Text>
-          </View>
-          <View style={styles.footerCard}>
-            <Text style={styles.footerCardTitle}>Seg</Text>
-            {imageIconCard}
-            <Text style={styles.footerCardPreview}>19°C</Text>
-          </View>
-          <View style={styles.footerCard}>
-            <Text style={styles.footerCardTitle}>Seg</Text>
-            {imageIconCard}
+            <SunNubled width={40} height={40} />
             <Text style={styles.footerCardPreview}>19°C</Text>
           </View>
         </ScrollView>
@@ -110,22 +121,28 @@ const styles = StyleSheet.create({
   content: {
     paddingTop: 50,
     alignItems: "center",
-    paddingHorizontal: 35,
+    paddingHorizontal: 30,
   },
   header: {
     width: "100%",
+    height: 45,
+    alignItems: "center",
+    padding: 10,
     flexDirection: "row",
     justifyContent: "space-between",
+    backgroundColor: "rgba(255,255,255, 0.23)",
+    borderRadius: 20,
   },
   headerLeft: {
     flexDirection: "row",
     alignItems: "center",
-    gap: 10,
+    gap: 8,
   },
   headerLeftText: {
     color: "#FFF",
     fontSize: 18,
     fontWeight: "600",
+    alignItems: "center",
   },
   info: {
     paddingVertical: 70,
@@ -175,7 +192,7 @@ const styles = StyleSheet.create({
     gap: 6,
     width: 99,
     height: 129,
-    backgroundColor: "rgba(255,255,255, 0.23)",
+    backgroundColor: "rgba(255,255,255, 0.30)",
     borderRadius: 8,
     marginRight: 14,
   },
